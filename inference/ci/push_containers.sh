8b938f0db63c7f1ffab8c7373f69f5be94908aa1
Kapil Vaswani <kapilv@microsoft.com>
2023-02-09 13:41:28 +0000

    deployment

---
+++ inference/ci/push_containers.sh
@@ -0,0 +1,4 @@
+docker push inference-server:latest $CONTAINER_REGISTRY/inference-server:latest
+docker push inference-proxy:latest $CONTAINER_REGISTRY/inference-proxy:latest
+docker push inference-init:latest $CONTAINER_REGISTRY/inference-init:latest
+docker push encfs:latest $CONTAINER_REGISTRY/encfs:latest
