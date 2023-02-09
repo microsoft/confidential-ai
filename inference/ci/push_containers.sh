2c5141095acba6778b3f90089c0d36d11d20fc70
Kapil Vaswani <kapilv@microsoft.com>
2023-02-09 15:53:19 +0000

    push containers

--- inference/ci/push_containers.sh
+++ inference/ci/push_containers.sh
@@ -1,5 +1,6 @@
-containers=('inference-server:latest' 'inference-proxy:latest' 'inference-init:latest' 'encfs:latest')
-for container in $containers; do
+containers=("inference-server:latest" "inference-proxy:latest" "inference-init:latest" "encfs:latest")
+for container in "${containers[@]}"
+do
   docker tag $container $CONTAINER_REGISTRY"/"$container
   docker push $CONTAINER_REGISTRY"/"$container
 done
