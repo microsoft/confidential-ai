c08040f6ad1d02cd7f8ad7c090270a541ebb26c7
Kapil Vaswani <kapilv@microsoft.com>
2023-02-09 17:24:44 +0000

    model signing and download

---
+++ inference/models/fetch_models.sh
@@ -0,0 +1,4 @@
+# ONNX densenet
+mkdir -p model_repository/densenet_onnx/1
+wget -O model_repository/densenet_onnx/1/model.onnx \
+     https://contentmamluswest001.blob.core.windows.net/content/14b2744cf8d6418c87ffddc3f3127242/9502630827244d60a1214f250e3bbca7/08aed7327d694b8dbaee2c97b8d0fcba/densenet121-1.2.onnx
