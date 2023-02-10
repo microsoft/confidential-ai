f7414913b81b0242e74ebd6b53d1e2646a0985cb
Kapil Vaswani <kapilv@microsoft.com>
2023-02-10 18:36:03 +0000

    fixes

--- inference/deployment/confidential-aci/generate-encrypted-filesystem-info.sh
+++ inference/deployment/confidential-aci/generate-encrypted-filesystem-info.sh
@@ -9,7 +9,7 @@ export MHSM_TOKEN=$(echo $MHSM_TOKEN | jq -r .accessToken)
 TMP=$(jq . encrypted-filesystem-config-template.json)
 
 TMP=`echo $TMP | \
-  jq '.azure_filesystems[0].azure_url = "https://" + env.AZURE_STORAGE_ACCOUNT_NAME + ".blob.core.windows.net/" + env.AZURE_MODEL_CONTAINER_NAME + "/model.img" + env.MODEL_SAS_TOKEN' | \
+  jq '.azure_filesystems[0].azure_url = "https://" + env.AZURE_STORAGE_ACCOUNT_NAME + ".blob.core.windows.net/" + env.AZURE_STORAGE_CONTAINER_NAME + "/model.img" + env.MODEL_SAS_TOKEN' | \
   jq '.azure_filesystems[0].mount_point = "/mnt/remote/models"' | \
   jq '.azure_filesystems[0].key.kid = "ModelFilesystemEncryptionKey"' | \
   jq '.azure_filesystems[0].key.mhsm.endpoint = env.AZURE_MHSM_ENDPOINT' | \
