e90a422b464508cb2c1e181b525e15f075642c7b
Stavros Volos <svolos@gmail.com>
2023-02-27 16:08:00 +0000

    Update set_policy.sh

--- inference/maa/set_policy.sh
+++ inference/maa/set_policy.sh
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 AZURE_MAA_ENDPOINT=`az attestation show --name $AZURE_MAA_CUSTOM_RESOURCE_NAME --resource-group $AZURE_RESOURCE_GROUP --query attestUri --output tsv | awk '{split($0,a,"//"); print a[2]}'`
 MAA_POLICY_URI="https://$AZURE_MAA_ENDPOINT/policies/SevSnpVm?api-version=2022-08-01"
 BEARER_TOKEN=`az account get-access-token --resource https://attest.azure.net --query accessToken --output tsv`
