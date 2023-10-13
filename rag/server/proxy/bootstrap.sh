e597f58a9efb5bcaa82ec1c7d456b7510a2a9934
Kapil Vaswani <kapilv@microsoft.com>
2023-10-13 09:28:25 +0000

    attestation

--- rag/server/proxy/bootstrap.sh
+++ rag/server/proxy/bootstrap.sh
@@ -11,6 +11,13 @@ openssl req -x509 -newkey rsa:2048 -keyout keys/server-key.pem -out certs/server
 # Extract sha256 hash of the certificate and format it
 export CERTIFICATE_HASH=$(openssl x509 -noout -sha256 -fingerprint -in certs/server-cert.pem | cut -f2 -d "=" |  sed -e "s/://g" | tr '[:upper:]' '[:lower:]')
 
+echo "Certficate hash $CERTIFICATE_HASH"
+
+# Generate attestation token 
+echo "Generating attestation token..."
+sudo ./confidential-computing-cvm-guest-attestation/cvm-attestation-sample-app/AttestationClient -n $CERTIFICATE_HASH -o token > ./attestation-token.txt
+cat attestation-token.txt
+
 # Obtain token validation certificate
 MAA_OPENID_RESPONSE=`curl -X GET https://$MAA_ENDPOINT/.well-known/openid-configuration`
 JWKS_URI=`echo $MAA_OPENID_RESPONSE | jq -r .jwks_uri`
